8c4d9aa41c2d6304d9440bbb2d396cb01228fcdb
Will Powell <me@willpowell.uk>
2024-08-07 10:50:39 +0100

    ecg - rr - acc working!

--- examples/ecg_rr_acc_recorder.py
+++ examples/ecg_rr_acc_recorder.py
@@ -20,7 +20,7 @@ async def scan():
         lambda dev, adv: dev.name and "polar" in dev.name.lower())
     return device
 
-async def run_ble_client(device, ecg_queue, hr_queue, acc_queue, quitclient):
+async def run_ble_client(device, ecg_acc_queue, hr_queue, quitclient):
     """ Connect to the BLE server and start ECG, HR, and ACC notifications. """
    
     def disconnected_callback(client):
@@ -35,7 +35,7 @@ async def run_ble_client(device, ecg_queue, hr_queue, acc_queue, quitclient):
             await asyncio.sleep(2)  # Add a 2-second delay
 
             # Start ECG notification
-            pmd = PolarMeasurementData(client, ecg_queue=ecg_queue)
+            pmd = PolarMeasurementData(client, ecg_queue=ecg_acc_queue, acc_queue=ecg_acc_queue)
             settings = await pmd.available_settings('ECG')
             logger.info("Request for available ECG settings returned the following:")
             for k, v in settings.items():
@@ -54,13 +54,12 @@ async def run_ble_client(device, ecg_queue, hr_queue, acc_queue, quitclient):
             await heartrate.start_notify()
 
             # Start ACC notification
-            acc_pmd = PolarMeasurementData(client, callback=lambda data: acc_queue.put_nowait(data))
-            settings = await acc_pmd.available_settings('ACC')
+            settings = await pmd.available_settings('ACC')
             logger.info("Request for available ACC settings returned the following:")
             for k, v in settings.items():
                 logger.info(f"{k}:\t{v}")
             try:
-                err_code, err_msg, *_ = await acc_pmd.start_streaming('ACC', RANGE=2, SAMPLE_RATE=25)
+                err_code, err_msg, *_ = await pmd.start_streaming('ACC', RANGE=2, SAMPLE_RATE=25)
                 if err_code != 0:
                     logger.error(f"PMD returned an error: {err_msg}")
                     sys.exit(err_code)
@@ -74,40 +73,12 @@ async def run_ble_client(device, ecg_queue, hr_queue, acc_queue, quitclient):
             if client.is_connected:
                 await pmd.stop_streaming('ECG')
                 await heartrate.stop_notify()
-                await acc_pmd.stop_streaming('ACC')
             
-            ecg_queue.put_nowait(('QUIT', None, None, None))
+            ecg_acc_queue.put_nowait(('QUIT', None, None, None))
             hr_queue.put_nowait(('QUIT', None, None, None))
-            acc_queue.put_nowait(('QUIT', None, None, None))
     except Exception as e:
         logger.error(f"Error in run_ble_client: {e}")
         quitclient.set()
-
-async def run_consumer_task_ecg(ecg_queue, file_path):
-    """Retrieve ECG data from the queue and store it in CSV files."""
-
-    # Ensure the directory exists
-    os.makedirs(file_path, exist_ok=True)
-    
-    # Define file paths for ECG data
-    file_path_ecg = os.path.join(file_path, 'ECG.csv')
-
-    # Open the file for writing
-    with open(file_path_ecg, 'w', newline='') as ecg_file:
-        ecg_writer = csv.writer(ecg_file)
-        
-        while True:
-            frame = await ecg_queue.get()
-            if frame[0] == 'QUIT':
-                break
-
-            timestamp_data, ecg_data = frame[1], frame[2]
-            
-            # Write each ECG sample to ECG.csv
-            for sample in ecg_data:
-                ecg_writer.writerow([sample])
-
-            logger.info(frame)
 
 async def run_consumer_task_hr(hr_queue, file_path):
     """Retrieve HR data from the queue and store it in a CSV file."""
@@ -130,28 +101,43 @@ async def run_consumer_task_hr(hr_queue, file_path):
             hr_writer.writerow([rr_intervals_str])
             logger.info(frame)
 
-async def run_consumer_task_acc(acc_queue, file_path):
-    """Retrieve ACC data from the queue and store it in a CSV file."""
+async def run_consumer_task_ecg_acc(ecg_acc_queue, file_path):
+    """Retrieve ECG/ACC data from the queue and store it in a CSV file."""
+
+    # Ensure the directory exists
+    os.makedirs(file_path, exist_ok=True)
+    
+    # Define file paths for ECG data
+    file_path_ecg = os.path.join(file_path, 'ECG.csv')
 
     # Define file path for ACC data
     file_path_acc = os.path.join(file_path, 'ACC.csv')
 
     # Open the file for writing
-    with open(file_path_acc, 'w', newline='') as acc_file:
+    with open(file_path_ecg, 'w', newline='') as ecg_file,\
+        open(file_path_acc, 'w', newline='') as acc_file:
+    
+        ecg_writer = csv.writer(ecg_file)
         acc_writer = csv.writer(acc_file)
 
         while True:
-            frame = await acc_queue.get()
-            if frame[0] == 'QUIT':  # intercept exit signal
-                break
-            
-            if frame[0] != 'ACC':
-                logger.error(f"Unexpected frame type: {frame[0]}")
-                continue
+            frame = await ecg_acc_queue.get()
+            type = frame[0]
+            match type:
+                case 'ECG':
+                    timestamp_data, ecg_data = frame[1], frame[2]
+                    for sample in ecg_data:
+                        ecg_writer.writerow([sample])
+                case 'ACC':
+                    timestamp_data, acc_data = frame[1], frame[2]
+                    for sample in acc_data:
+                        acc_writer.writerow([*sample])
+                case 'QUIT':
+                    break
+                case _:
+                    logger.error(f"Unexpected frame type: {frame[0]}")
+                    continue
 
-            tstamp, acc_data = frame[1], frame[2]
-            for sample in acc_data:
-                acc_writer.writerow([tstamp, *sample])
             logger.info(frame)
 
 def input_thread(quitclient):
@@ -165,17 +151,15 @@ async def main(file_path):
     if device is None:
         logger.error("Polar device not found.")
         sys.exit(-4)
-    ecg_queue = asyncio.Queue()
+    ecg_acc_queue = asyncio.Queue()
     hr_queue = asyncio.Queue()
-    acc_queue = asyncio.Queue()
     quitclient = asyncio.Event()
     input_thread_obj = threading.Thread(target=input_thread, args=(quitclient,))
     input_thread_obj.start()
-    producer = run_ble_client(device, ecg_queue, hr_queue, acc_queue, quitclient)
-    consumer_ecg = run_consumer_task_ecg(ecg_queue, file_path)
+    producer = run_ble_client(device, ecg_acc_queue, hr_queue, quitclient)
+    consumer_ecg_acc = run_consumer_task_ecg_acc(ecg_acc_queue, file_path)
     consumer_hr = run_consumer_task_hr(hr_queue, file_path)
-    consumer_acc = run_consumer_task_acc(acc_queue, file_path)
-    await asyncio.gather(producer, consumer_ecg, consumer_hr, consumer_acc)
+    await asyncio.gather(producer, consumer_ecg_acc, consumer_hr)
     logger.info("Bye.")
     input_thread_obj.join()
 
